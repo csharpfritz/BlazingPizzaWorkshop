661a655079beb0d45db0974dbcd1deaa54bc4d31
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-03-05 22:13:09 -0500

    First commit

---
+++ BlazingPizza.Shared/Topping.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazingPizza.Shared;
+
+public class Topping
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public string GetFormattedPrice() => Price.ToString("0.00");
+
+}
---
+++ BlazingPizza.Shared/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingPizza.Shared;
+
+public class UserInfo
+{
+    public bool IsAuthenticated { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+}
